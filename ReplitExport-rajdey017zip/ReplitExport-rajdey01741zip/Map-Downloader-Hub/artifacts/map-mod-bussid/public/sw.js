fab1467ca51333342a31d3cef4e5de0f319e8bc1
Raj4148 <rajdey017@gmail.com>
2026-09-21 22:05:51 +0600

    Update service worker with Monetag Push Notifications setup

--- ReplitExport-rajdey017zip/ReplitExport-rajdey01741zip/Map-Downloader-Hub/artifacts/map-mod-bussid/public/sw.js
+++ ReplitExport-rajdey017zip/ReplitExport-rajdey01741zip/Map-Downloader-Hub/artifacts/map-mod-bussid/public/sw.js
@@ -1,1 +1,6 @@
-// Service worker cleared
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11854737
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
